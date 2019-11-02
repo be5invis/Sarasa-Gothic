899a6a51ffde3d3bbdc7f8301d1d0b7b6ec17c1c
be5invis <belleve@typeof.net>
2019-11-01 20:27:35 -0700

    Implement continuous dash. Fixes #84.

--- make/punct/common.js
+++ make/punct/common.js
@@ -98,6 +98,70 @@ exports.sanitizeSymbols = async function sanitizeSymbols(isType) {
 	}
 };
 
+exports.buildNexusDash = async function() {
+	let gidCovered = new Set();
+	for (const u of [0x2013, 0x2014, 0x2015]) {
+		const gn = this.find.gname.unicode(u);
+		if (gn) gidCovered.add(gn);
+	}
+
+	const nexusLookupName = "ccmp__nexusDash";
+	let nexusLookupSubst = {};
+	const nexusLookup = { type: "gsub_single", subtables: [nexusLookupSubst] };
+	const nexusChainingLookupName = "ccmp__nexusDash_chaining";
+	let nexusChainingRules = [];
+	const nexusChainingLookup = { type: "gsub_chaining", subtables: nexusChainingRules };
+
+	for (const originalGid of gidCovered) {
+		const glyph = createNexusGlyph(this.find.glyph$(originalGid));
+		const nexusGid = originalGid + ".nexus";
+		await this.save.to(nexusGid, null, glyph);
+		nexusLookupSubst[originalGid] = nexusGid;
+		nexusChainingRules.push({
+			match: [[originalGid, nexusGid], [originalGid]],
+			apply: [{ lookup: nexusLookupName, at: 1 }],
+			inputBegins: 1,
+			inputEnds: 2
+		});
+	}
+
+	if (this.font.GSUB) {
+		this.font.GSUB.lookups[nexusLookupName] = nexusLookup;
+		this.font.GSUB.lookups[nexusChainingLookupName] = nexusChainingLookup;
+
+		for (const fid in this.font.GSUB.features) {
+			if (fid.slice(0, 4) !== "ccmp") continue;
+			const feature = this.font.GSUB.features[fid];
+			if (!feature) continue;
+			feature.push(nexusChainingLookupName);
+		}
+	}
+};
+
+function createNexusGlyph(glyph) {
+	let xMax = -0xffff,
+		xMin = 0xffff;
+	if (glyph.contours) {
+		for (let c of glyph.contours) {
+			for (let z of c) {
+				if (z.x > xMax) xMax = z.x;
+				if (z.x < xMin) xMin = z.x;
+			}
+		}
+	}
+	const rsb = glyph.advanceWidth - xMax;
+	const negMin = rsb * 1.5;
+	const scaling = (xMax + rsb * 1.5) / (xMax - xMin);
+	if (glyph.contours) {
+		for (let c of glyph.contours) {
+			for (let z of c) {
+				z.x = (z.x - xMin) * scaling - negMin;
+			}
+		}
+	}
+	return glyph;
+}
+
 function removeUnusedFeature(table, tag) {
 	if (!table) return;
 	for (let f in table.features) {
@@ -123,6 +187,38 @@ exports.removeUnusedFeatures = function(a, mono) {
 		removeUnusedFeature(a.GPOS, "vpal");
 	}
 };
+
+exports.removeDashCcmp = function(a, mono) {
+	if (!a.GSUB || !a.GSUB.features || !a.GSUB.lookups) return;
+
+	let affectedLookups = new Set();
+	for (const fid in a.GSUB.features) {
+		if (fid.slice(0, 4) === "ccmp") {
+			const feature = a.GSUB.features[fid];
+			if (!feature) continue;
+			for (const lid of feature) affectedLookups.add(lid);
+		}
+	}
+
+	for (const lid of affectedLookups) {
+		const lookup = a.GSUB.lookups[lid];
+		removeDashCcmpLookup(lookup, a.cmap);
+	}
+};
+function removeDashCcmpLookup(lookup, cmap) {
+	if (!lookup || lookup.type !== "gsub_ligature") return;
+	for (const st of lookup.subtables) {
+		let st1 = [];
+		for (const subst of st.substitutions) {
+			let valid = true;
+			for (const gid of subst.from) {
+				if (cmap[0x2014] === gid || cmap[0x2015] === gid) valid = false;
+			}
+			if (valid) st1.push(subst);
+		}
+		st.substitutions = st1;
+	}
+}
 
 exports.toPWID = async function() {
 	const font = this.font;
