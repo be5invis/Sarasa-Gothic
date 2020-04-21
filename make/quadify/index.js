b0ae6b14c820232955d2fb4735c8191500af22d3
be5invis <belleve@typeof.net>
2020-04-20 20:13:06 -0700

    Increase quadify precision

--- make/quadify/index.js
+++ make/quadify/index.js
@@ -4,7 +4,7 @@ const { introduce, build, quadify } = require("megaminx");
 
 async function pass(ctx, config, argv) {
 	await ctx.run(introduce, "a", { from: argv.main, prefix: "a", ignoreHints: true });
-	await ctx.run(quadify, "a");
+	await ctx.run(quadify, "a", { error: 1 / 4 });
 	await ctx.run(build, "a", { to: config.o, optimize: true });
 	ctx.remove("a");
 }
