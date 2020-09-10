1d83b5bd16e1e39d9327458474b451c87f745321
be5invis <belleve@typeof.net>
2020-09-10 00:41:46 -0700

    Fixup typo (#185)

--- make/pass1/tnum.js
+++ make/pass1/tnum.js
@@ -4,6 +4,6 @@ exports.toTNUM = async function () {
 	const font = this.font;
 	for (let c in font.cmap) {
 		if (!font.cmap[c]) continue;
-		font.cmap[c] = this.find.gname.subst("pwid", font.cmap[c]);
+		font.cmap[c] = this.find.gname.subst("tnum", font.cmap[c]);
 	}
 };
