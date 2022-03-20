2400ffcdff9d1bdce9461f8f173812956737cde6
be5invis <belleve@typeof.net>
2022-03-19 21:13:54 -0700

    Release 0.36.1

--- checkenv.js
+++ checkenv.js
@@ -1,14 +1,13 @@
 "use strict";
 
 const which = require("which");
-const colors = require("colors/safe");
 
 function check(util) {
 	try {
 		which.sync(util);
-		console.error(colors.green(`External dependency <${util}> is present.`));
+		console.error(`External dependency <${util}> is present.`);
 	} catch (e) {
-		console.error(colors.red(`External dependency <${util}> not found.`));
+		console.error(`External dependency <${util}> not found.`);
 	}
 }
 
